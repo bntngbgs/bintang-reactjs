ed7dd53355fede55041c8cf72536a895981123db
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2024-10-09 17:23:30 +0700

    added paragraph

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello from me, start learning react js</p>
         <a
           className="App-link"
           href="https://reactjs.org"
